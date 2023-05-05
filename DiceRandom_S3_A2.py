b28121fd6fe4d2ed2a1bae467548ea0c221ecb0a
LalehMashayekhi <132183349+LalehMashayekhi@users.noreply.github.com>
2023-05-05 23:16:36 +0330

    Add files via upload

---
+++ DiceRandom_S3_A2.py
@@ -0,0 +1,16 @@
+import random
+while True:
+    dice=random.randint(1,6)
+    print("Dice : ",dice)
+    if dice==6:
+        continue
+    else:
+        break
+            
+
+    
+    
+
+    
+    
+      
